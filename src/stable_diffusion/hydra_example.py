18069b58f146a7ccace88ab51e281492e5fa74a4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:56:07 +0100

    Work in progress

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -3,8 +3,6 @@ from os.path import join, exists, dirname, abspath, basename, relpath
 from os import getcwd, pardir
 from omegaconf import DictConfig, OmegaConf
 
-def print_log(text: str): print(f"[ log ]: {text}")
-def print_error(text: str): print(f"[ error ]: {text}")
 
 parent_dir = abspath(join(join(join(abspath(__file__), pardir), pardir), pardir))
 config_dir = join(parent_dir, "configs")
