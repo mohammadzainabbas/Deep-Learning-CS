8cc6355f9e6877b25172e8f46ba55cae1848dfb3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:58:35 +0100

    Minor

--- src/stable_diffusion/cross_attention.py
+++ src/stable_diffusion/cross_attention.py
@@ -322,5 +322,9 @@ eyesclosed = stablediffusion(
   )
 display(*[init_img, eyesclosed])
 
+@hydra.main(config_path=join(config_dir, "stable_diffusion"), config_name="stable_diffusion", version_base=None)
+def main(conf: DictConfig) -> None:
+    print_log(f"{conf = }")
 
-
+if __name__ == "__main__":
+    main()
