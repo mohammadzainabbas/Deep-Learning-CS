dcce41d86be6e1adb14b8254ec24f83060c80143
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:49:02 +0100

    Minor change

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -7,8 +7,9 @@ def print_error(text: str): print(f"[ error ]: {text}")
 
 @hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
-
     print_log(f"{conf = }")
+    conf.get()
+
 
 if __name__ == "__main__":
     main()
