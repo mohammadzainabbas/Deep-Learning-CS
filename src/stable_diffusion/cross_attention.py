32a90356aa84a84bc62bec0c452238837d1eb988
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:44:27 +0100

    Minor change

--- src/stable_diffusion/cross_attention.py
+++ src/stable_diffusion/cross_attention.py
@@ -329,7 +329,7 @@ from omegaconf import DictConfig, OmegaConf
 def print_log(text: str): print(f"[ log ]: {text}")
 def print_error(text: str): print(f"[ error ]: {text}")
 
-hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base="1.1")
+hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
 
     config = OmegaConf.to_yaml(conf, resolve=True)
