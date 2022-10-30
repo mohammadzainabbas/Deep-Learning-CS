d3979c6f169a53f1038e2d55035d60c72d7581b5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:50:27 +0100

    Minor update

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -10,8 +10,6 @@ config_dir = join(hydra.utils.get_original_cwd(), "config")
 @hydra.main(config_path=join(config_dir, "stable_diffusion"), config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
     print_log(f"{conf = }")
-    conf.get()
-
 
 if __name__ == "__main__":
     main()
