7612ea9cd20ab1f8825618f89fe0e713d46a1252
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:50:06 +0100

    Minor change

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -5,7 +5,9 @@ from omegaconf import DictConfig, OmegaConf
 def print_log(text: str): print(f"[ log ]: {text}")
 def print_error(text: str): print(f"[ error ]: {text}")
 
-@hydra.main(config_path=join("..", "..", "configs", "stable_diffusion"), config_name="stable_diffusion", version_base=None)
+config_dir = join(hydra.utils.get_original_cwd(), "config")
+
+@hydra.main(config_path=join(config_dir, "stable_diffusion"), config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
     print_log(f"{conf = }")
     conf.get()
