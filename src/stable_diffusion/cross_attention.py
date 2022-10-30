a19718429cc5a593eacb271c622d1149eeaea6dd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:45:09 +0100

    Updated code

--- src/stable_diffusion/cross_attention.py
+++ src/stable_diffusion/cross_attention.py
@@ -322,20 +322,5 @@
 #   )
 # display(*[init_img, eyesclosed])
 
-import hydra
-from os.path import join
-from omegaconf import DictConfig, OmegaConf
-
-def print_log(text: str): print(f"[ log ]: {text}")
-def print_error(text: str): print(f"[ error ]: {text}")
-
-hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
-def main(conf: DictConfig) -> None:
-
-    config = OmegaConf.to_yaml(conf, resolve=True)
-    print_log(f"{config = }")
-
-if __name__ == "__main__":
-    main()
 
 
