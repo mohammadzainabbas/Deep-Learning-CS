e48b7246bcb20001b42841ad169338596e2e9977
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:47:59 +0100

    Updated code

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -9,9 +9,6 @@ def print_error(text: str): print(f"[ error ]: {text}")
 def main(conf: DictConfig) -> None:
 
     print_log(f"{conf = }")
-    config = OmegaConf.to_yaml(conf, resolve=True)
-    print_log(f"{config = }")
-    print(config)
 
 if __name__ == "__main__":
     main()
