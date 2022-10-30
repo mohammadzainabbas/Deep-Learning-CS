cd9026fc0137df6a197b4ec2352df2de9302401c
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:46:56 +0100

    Work in progress

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -1,26 +1,16 @@
-# import hydra
-# from os.path import join
-# from omegaconf import DictConfig, OmegaConf
-
-# def print_log(text: str): print(f"[ log ]: {text}")
-# def print_error(text: str): print(f"[ error ]: {text}")
-
-# hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
-# def main(conf: DictConfig) -> None:
-
-#     config = OmegaConf.to_yaml(conf, resolve=True)
-#     print_log(f"{config = }")
-
-# if __name__ == "__main__":
-#     main()
-
-
-from omegaconf import DictConfig, OmegaConf
 import hydra
+from os.path import join
+from omegaconf import DictConfig, OmegaConf
 
-@hydra.main(version_base=None, config_path=".", config_name="config")
-def my_app(cfg):
-    print(OmegaConf.to_yaml(cfg))
+def print_log(text: str): print(f"[ log ]: {text}")
+def print_error(text: str): print(f"[ error ]: {text}")
+
+@hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
+def main(conf: DictConfig) -> None:
+
+    print_log(f"{config = }")
+    config = OmegaConf.to_yaml(conf, resolve=True)
+    print_log(f"{config = }")
 
 if __name__ == "__main__":
-    my_app()
+    main()
