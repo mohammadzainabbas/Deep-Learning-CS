85e17e1ce82e442cf40702901205aaf6f14e04c1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:46:13 +0100

    Minor

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -1,15 +1,26 @@
-import hydra
-from os.path import join
+# import hydra
+# from os.path import join
+# from omegaconf import DictConfig, OmegaConf
+
+# def print_log(text: str): print(f"[ log ]: {text}")
+# def print_error(text: str): print(f"[ error ]: {text}")
+
+# hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
+# def main(conf: DictConfig) -> None:
+
+#     config = OmegaConf.to_yaml(conf, resolve=True)
+#     print_log(f"{config = }")
+
+# if __name__ == "__main__":
+#     main()
+
+
 from omegaconf import DictConfig, OmegaConf
+import hydra
 
-def print_log(text: str): print(f"[ log ]: {text}")
-def print_error(text: str): print(f"[ error ]: {text}")
-
-hydra.main(config_path="../../configs/stable_diffusion", config_name="stable_diffusion", version_base=None)
-def main(conf: DictConfig) -> None:
-
-    config = OmegaConf.to_yaml(conf, resolve=True)
-    print_log(f"{config = }")
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def my_app(cfg):
+    print(OmegaConf.to_yaml(cfg))
 
 if __name__ == "__main__":
-    main()
+    my_app()
