6d51e7e9db2c76334d158799944ba363ae0c443f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:55:46 +0100

    Minor

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -7,7 +7,7 @@ def print_log(text: str): print(f"[ log ]: {text}")
 def print_error(text: str): print(f"[ error ]: {text}")
 
 parent_dir = abspath(join(join(join(abspath(__file__), pardir), pardir), pardir))
-config_dir = join(parent_dir, "config")
+config_dir = join(parent_dir, "configs")
 
 @hydra.main(config_path=join(config_dir, "stable_diffusion"), config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
