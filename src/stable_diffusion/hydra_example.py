2d5d05929e0dc95c16b46cd2c6d0e09769842463
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2022-10-30 14:49:45 +0100

    Minor

--- src/stable_diffusion/hydra_example.py
+++ src/stable_diffusion/hydra_example.py
@@ -5,7 +5,7 @@ from omegaconf import DictConfig, OmegaConf
 def print_log(text: str): print(f"[ log ]: {text}")
 def print_error(text: str): print(f"[ error ]: {text}")
 
-@hydra.main(config_path=join("..", "..", "configs", "stable_diffusion", config_name="stable_diffusion", version_base=None)
+@hydra.main(config_path=join("..", "..", "configs", "stable_diffusion"), config_name="stable_diffusion", version_base=None)
 def main(conf: DictConfig) -> None:
     print_log(f"{conf = }")
     conf.get()
